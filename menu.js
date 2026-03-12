908daab354de28c7b73f50148d5c88ad7e4ab955
bhargavibayar02 <168254235+bhargavibayar02@users.noreply.github.com>
2026-03-12 23:49:53 +0530

    Add toggleMenu function for sidebar interaction

---
+++ menu.js
@@ -0,0 +1,9 @@
+function toggleMenu(){
+
+const sidebar = document.querySelector(".sidebar");
+const hamburger = document.querySelector(".hamburger");
+
+sidebar.classList.toggle("active");
+hamburger.classList.toggle("active");
+
+}
