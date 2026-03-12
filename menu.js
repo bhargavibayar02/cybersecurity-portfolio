0460a47bea160e8649a413f1b51d4094f6170b7e
bhargavibayar02 <168254235+bhargavibayar02@users.noreply.github.com>
2026-03-12 23:54:22 +0530

    Update menu.js

--- menu.js
+++ menu.js
@@ -1,9 +1,9 @@
-function toggleMenu(){
+const hamburger = document.getElementById("hamburger");
+const sidebar = document.getElementById("sidebar");
 
-const sidebar = document.querySelector(".sidebar");
-const hamburger = document.querySelector(".hamburger");
+hamburger.addEventListener("click", function(){
 
 sidebar.classList.toggle("active");
 hamburger.classList.toggle("active");
 
-}
+});
